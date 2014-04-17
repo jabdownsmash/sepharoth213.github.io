083d8e6a7b2a3e8e699eee788f2a0bf072df2d60
Aaron Santiago <aaronsantiago@gmail.com>
2014-04-16 20:47:08 -0400

    aiient

--- srcsurvival/surviveSRC.js
+++ srcsurvival/surviveSRC.js
@@ -447,7 +447,7 @@ function Enemy() {
 Enemy.prototype.draw = function(context) {
   Game.context.fillStyle = 'red';
   
-  this.beatDistance = 1 - ((document.getElementById('soundEfx').currentTime % (82/120))/(82/120))
+  this.beatDistance = 1 - ((document.getElementById('soundEfx').currentTime % (80/120))/(80/120))
   this.beatDistance = this.beatDistance * 30
 
   console.log(this.beatDistance)
